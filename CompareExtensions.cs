bd7cd477d0704d542fa9587620b8915b95fd553a
Alexsander Faria <alexsander.itj@hotmail.com>
2022-05-28 16:09:43 -0300

    Adicionar arquivos de projeto.

---
+++ CompareExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLib
+{
+    public static class CompareExtensions
+    {
+        /// <summary>
+        /// verificar se o numero é par (ou nao)
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static bool IsEven(double n) => (n % 2 == 0);
+    }
+}
---
+++ ConvertExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace CommonLib
+{
+    public static class ConvertExtensions
+    {
+        /// <summary>
+        /// Converte um número em um caracter correspondente a tabela ascii
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public static async Task<char> DecodeAscii(this Task<int> i) => await Task.Run(async () => Convert.ToChar(await i));
+
+        #region[CONVERT TO INT32]
+
+        public static int ToInt(this decimal num) => Convert.ToInt32(num);
+
+        public static int ToInt(this string num) => Convert.ToInt32(num);
+
+        #endregion
+
+        #region[CONVERT TO INT16]]
+
+        public static short ToShort(this string num) => Convert.ToInt16(num);
+
+        #endregion
+
+
+        #region[CONVERT TO INT64]
+        public static long ToLong(this string num) => Convert.ToInt64(num);
+
+        #endregion
+    }
+}
